70e99df3afb696f91406cd1c5a249b0899e7a4cb
saracoglumert <70754740+saracoglumert@users.noreply.github.com>
2023-11-12 12:11:57 +0100

    new structure

--- math_quiz/tests_math_quiz.py
+++ math_quiz/tests_math_quiz.py
@@ -1,5 +1,5 @@
 import unittest
-from math_quiz import function_A, function_B, function_C
+from math_quiz import randInt, randOper, generateProblem
 
 
 class TestMathGame(unittest.TestCase):
@@ -9,22 +9,27 @@ class TestMathGame(unittest.TestCase):
         min_val = 1
         max_val = 10
         for _ in range(1000):  # Test a large number of random values
-            rand_num = function_A(min_val, max_val)
+            rand_num = randInt(min_val, max_val)
             self.assertTrue(min_val <= rand_num <= max_val)
 
     def test_function_B(self):
-        # TODO
+        for _ in range(1000):
+             output = randOper()
+             self.assertTrue(output in ['+', '-', '*'])
         pass
 
     def test_function_C(self):
             test_cases = [
                 (5, 2, '+', '5 + 2', 7),
-                ''' TODO add more test cases here '''
+                (4, 3, '-', '4 - 3', 1),
+                (6, 8, '*', '6 + 8', 48),
             ]
 
             for num1, num2, operator, expected_problem, expected_answer in test_cases:
-                # TODO
-                pass
+                tmp_prob, tmp_ans = generateProblem(num1,num2,operator)
+                self.assertTrue(tmp_ans == expected_answer and tmp_prob == expected_problem)
 
 if __name__ == "__main__":
     unittest.main()
+
+
