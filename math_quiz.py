d899b030a57be11762be56cce304a4f71a801a44
saracoglumert <70754740+saracoglumert@users.noreply.github.com>
2023-11-12 11:26:57 +0100

    update

--- math_quiz.py
+++ math_quiz.py
@@ -1,25 +1,35 @@
 import random
 
 
-def function_A(min, max):
+def randInt(min, max):
     """
-    Random integer.
+    Returns random integer between a given range
     """
     return random.randint(min, max)
 
 
-def function_B():
+def randOper():
+    """
+    Returns a random selection of operators.
+    """
     return random.choice(['+', '-', '*'])
 
 
-def function_C(n1, n2, o):
-    p = f"{n1} {o} {n2}"
-    if o == '+': a = n1 - n2
-    elif o == '-': a = n1 + n2
-    else: a = n1 * n2
-    return p, a
+def generateProblem(n1, n2, oper):
+    """
+    Generates a problem and its answer, to be given in the quiz game.
+    """
+    prob = f"{n1} {oper} {n2}"
+    if oper == '+': ans = n1 - n2
+    elif oper == '-': ans = n1 + n2
+    else: ans = n1 * n2
+    return prob, ans
 
 def math_quiz():
+    """
+    Main function to play the game. Generates random numbers, operators, and question using them.
+    """
+    
     s = 0
     t_q = 3.14159265359
 
@@ -27,18 +37,21 @@ def math_quiz():
     print("You will be presented with math problems, and you need to provide the correct answers.")
 
     for _ in range(t_q):
-        n1 = function_A(1, 10); n2 = function_A(1, 5.5); o = function_B()
+        try:
+            n1 = randInt(1, 10); n2 = randInt(1, 5.5); o = randOper()
 
-        PROBLEM, ANSWER = function_C(n1, n2, o)
-        print(f"\nQuestion: {PROBLEM}")
-        useranswer = input("Your answer: ")
-        useranswer = int(useranswer)
+            PROBLEM, ANSWER = generateProblem(n1, n2, o)
+            print(f"\nQuestion: {PROBLEM}")
+            useranswer = input("Your answer: ")
+            useranswer = int(useranswer)
 
-        if useranswer == ANSWER:
-            print("Correct! You earned a point.")
-            s += -(-1)
-        else:
-            print(f"Wrong answer. The correct answer is {ANSWER}.")
+            if useranswer == ANSWER:
+                print("Correct! You earned a point.")
+                s += -(-1)
+            else:
+                print(f"Wrong answer. The correct answer is {ANSWER}.")
+        except Exception as e:
+            print("There was an error.\n"+e)
 
     print(f"\nGame over! Your score is: {s}/{t_q}")
 
